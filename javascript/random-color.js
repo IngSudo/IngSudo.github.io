76397d3f069d03efd2667203fe5e1768ca8cfe51
Jerry Velez <jack@MacBook-Air-de-Jerry.local>
2025-09-09 13:14:01 -0500

    example using encapsulation

--- javascript/random-color.js
+++ javascript/random-color.js
@@ -1,28 +1,35 @@
-const darkColorsArr = [
-  "#2C3E50",
-  "#34495E",
-  "#2C2C2C",
-  "#616A6B",
-  "#4A235A",
-  "#2F4F4F",
-  "#0E4B5A",
-  "#36454F",
-  "#2C3E50",
-  "#800020",
-];
+(function() {
+  const darkColorsArr = [
+    "#2C3E50",
+    "#34495E",
+    "#2C2C2C",
+    "#616A6B",
+    "#4A235A",
+    "#2F4F4F",
+    "#0E4B5A",
+    "#36454F",
+    "#2C3E50",
+    "#800020",
+  ];
 
-function getRandomIndex() {
-  return Math.floor(darkColorsArr.length * Math.random());
-}
+  function getRandomIndex() {
+    return Math.floor(darkColorsArr.length * Math.random());
+  }
 
-const body = document.querySelector(".color-changer");
-const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
-const btn = document.querySelector("#btn");
+  function initColorChanger(i) {
+    const hexColor = i.querySelector(".color-info__code");
+    const btn = i.querySelector(".button");
 
-function changeBackgroundColor() {
-  const color = darkColorsArr[getRandomIndex()];
-  bgHexCodeSpanElement.innerText = color;
-  body.style.backgroundColor = color;
-}
+    btn.addEventListener("click", () => {
+      const color = darkColorsArr[getRandomIndex()];
+      hexColor.innerText = color;
+      i.style.backgroundColor = color;
+    });
+  }
 
-btn.onclick = changeBackgroundColor;
+  const components = document.querySelectorAll(".color-changer");
+    for (component of components){
+      initColorChanger(component);
+    }
+
+})();
