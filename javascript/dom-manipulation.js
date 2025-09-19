4a2b6aa233ac8ef428cc7d00256e69fba27fff29
Jerry Velez <jack@MacBook-Air-de-Jerry.local>
2025-09-19 13:50:42 -0500

    class and objects

--- javascript/dom-manipulation.js
+++ javascript/dom-manipulation.js
@@ -1,11 +1,41 @@
-(function () {
-  function getRandomComputerResult() {
+class InitGame {
+  constructor(game) {
+    this.game = game;
+
+    this.playerScore = 0;
+    this.computerScore = 0;
+
+    const scoreValues = this.game.querySelectorAll(".rps-game__score-value");
+    this.playerScoreSpan = scoreValues[0];
+    this.computerScoreSpan = scoreValues[1];
+
+    this.roundResultsMsg = this.game.querySelector(".rps-game__results-msg");
+    this.winnerMsg = this.game.querySelector(".rps-game__results-winner");
+    this.optionsContainer = this.game.querySelector(".rps-game__options");
+    this.resetGameBtn = this.game.querySelector(
+      ".rps-game__results .rps-game__btn"
+    );
+
+    this.resetGameBtn.addEventListener("click", () => this.resetGame());
+
+    const optionBtns = this.game.querySelectorAll(
+      ".rps-game__options-btns .rps-game__btn"
+    );
+
+    optionBtns.forEach((btn) => {
+      btn.addEventListener("click", () =>
+        this.showResults(btn.textContent.trim())
+      );
+    });
+  }
+
+  getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
   }
 
-  function hasPlayerWonTheRound(player, computer) {
+  hasPlayerWonTheRound(player, computer) {
     return (
       (player === "Rock" && computer === "Scissors") ||
       (player === "Scissors" && computer === "Paper") ||
@@ -13,74 +43,47 @@
     );
   }
 
-  function initGame(game) {
-    let playerScore = 0;
-    let computerScore = 0;
+  getRoundResults(userOption) {
+    const computerResult = this.getRandomComputerResult();
 
-    const scoreValues = game.querySelectorAll(".rps-game__score-value");
-    const playerScoreSpan = scoreValues[0];
-    const computerScoreSpan = scoreValues[1];
-
-    const roundResultsMsg = game.querySelector(".rps-game__results-msg");
-    const winnerMsg = game.querySelector(".rps-game__results-winner");
-    const optionsContainer = game.querySelector(".rps-game__options");
-    const resetGameBtn = game.querySelector(".rps-game__results .rps-game__btn");
-
-    function getRoundResults(userOption) {
-      const computerResult = getRandomComputerResult();
-
-      if (hasPlayerWonTheRound(userOption, computerResult)) {
-        playerScore++;
-        return `Player wins! ${userOption} beats ${computerResult}`;
-      } else if (computerResult === userOption) {
-        return `It's a tie! Both chose ${userOption}`;
-      } else {
-        computerScore++;
-        return `Computer wins! ${computerResult} beats ${userOption}`;
-      }
+    if (this.hasPlayerWonTheRound(userOption, computerResult)) {
+      this.playerScore++;
+      return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+      return `It's a tie! Both chose ${userOption}`;
+    } else {
+      this.computerScore++;
+      return `Computer wins! ${computerResult} beats ${userOption}`;
     }
-
-    function showResults(userOption) {
-      roundResultsMsg.innerText = getRoundResults(userOption);
-      computerScoreSpan.innerText = computerScore;
-      playerScoreSpan.innerText = playerScore;
-
-      if (playerScore === 3 || computerScore === 3) {
-        winnerMsg.innerText = `${
-          playerScore === 3 ? "Player" : "Computer"
-        } has won the game!`;
-
-        resetGameBtn.style.display = "block";
-        optionsContainer.style.display = "none";
-      }
-    }
-
-    function resetGame() {
-      playerScore = 0;
-      computerScore = 0;
-      playerScoreSpan.innerText = playerScore;
-      computerScoreSpan.innerText = computerScore;
-      resetGameBtn.style.display = "none";
-      optionsContainer.style.display = "block";
-      winnerMsg.innerText = "";
-      roundResultsMsg.innerText = "";
-    }
-
-    resetGameBtn.addEventListener("click", resetGame);
-
-    const optionBtns = game.querySelectorAll(
-      ".rps-game__options-btns .rps-game__btn"
-    );
-
-    optionBtns.forEach((btn) => {
-      btn.addEventListener("click", () =>
-        showResults(btn.textContent.trim())
-      );
-    });
   }
 
-  const components = document.querySelectorAll(".rps-game");
-  for (const component of components) {
-    initGame(component);
+  showResults(userOption) {
+    this.roundResultsMsg.innerText = this.getRoundResults(userOption);
+    this.computerScoreSpan.innerText = this.computerScore;
+    this.playerScoreSpan.innerText = this.playerScore;
+
+    if (this.playerScore === 3 || this.computerScore === 3) {
+      this.winnerMsg.innerText = `${
+        this.playerScore === 3 ? "Player" : "Computer"
+      } has won the game!`;
+
+      this.resetGameBtn.style.display = "block";
+      this.optionsContainer.style.display = "none";
+    }
   }
-})();
+
+  resetGame() {
+    this.playerScore = 0;
+    this.computerScore = 0;
+    this.playerScoreSpan.innerText = this.playerScore;
+    this.computerScoreSpan.innerText = this.computerScore;
+    this.resetGameBtn.style.display = "none";
+    this.optionsContainer.style.display = "block";
+    this.winnerMsg.innerText = "";
+    this.roundResultsMsg.innerText = "";
+  }
+}
+
+document.querySelectorAll(".rps-game").forEach((component) => {
+  new InitGame(component);
+});
